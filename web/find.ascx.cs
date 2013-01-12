7a65974d37b6f2e406619afc5867a76989362a5a
bobyang_cp <SND\bobyang_cp>
2013-01-12 02:12:23 -0800

    add json

--- web/find.ascx.cs
+++ web/find.ascx.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+
 
 namespace web
 {
     public partial class find : System.Web.UI.UserControl
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
+        //protected void Page_Load(object sender, EventArgs e)
+        //{
+        //    class Customer
+        //    {
+        //        public String Name { get; set; }
+        //        public String Description { get; set; }
+        //        public int Status { get; set; }
+        //    }
+        //    Customer customer = JsonConvert.DeserializeObject<Customer>(jsonContent);
+        //}
     }
 }
